95741a01deb978d8e0abe242dd388930b0ffe754
Silvia <you@example.com>
2023-04-13 13:01:58 +0200

    Added PCG table generation

---
+++ 1_datasets/generate_PCGtable.py
@@ -0,0 +1,94 @@
+def generate_PCGtable(N, Av, L=None, LN=None, outfile=None):
+    """
+    Generate a table with N rows, where each row represents a group with an
+    average value and a number of leaves.
+
+    Args:
+        N (int): Number of groups.
+        Av (list of floats): List of average values for each group. The length
+            of the list must be equal to N, and the values must sum to 1.
+        L (list of ints, optional): List of number of leaves for each group. If this
+            argument is provided, it takes precedence over the L argument. Default value
+            is None.
+        LN (int, optional): Total number of leaves. If this argument is provided,
+            the number of leaves for each group will be randomly generated. Default
+            value is None.
+        outfile (str, optional): Output file path. If provided, the generated table
+            will be saved in CSV format at the specified location. Default value is None.
+
+    Returns:
+        list of lists: A list of N+1 lists, where each list represents a row in the
+        generated table. The first two elements of each row are the group name and the
+        average value, respectively. The third element is either the number of leaves
+        for the group or a semicolon-separated string of leaf names. The last row of the
+        table contains the total number of leaves.
+
+    Raises:
+        ValueError: If both L and LN arguments are None.
+    
+    If LN argument is None, the function randomly generates the number of leaves for
+    each group. The total number of leaves is determined by the L argument. The function
+    ensures that each group has at least one leaf and the total number of leaves is
+    distributed evenly among the groups. The number of leaves for the last group is
+    the remainder of the division, and is added to the last row of the table as the
+    total number of leaves.
+    
+    Example usage:
+	N = 3
+	Av = [0.2, 0.3, 0.5]
+	LN = 10
+
+	table = generate_table(N, Av, LN=LN)
+
+	for row in table:
+	    print("\t".join(str(x) for x in row))
+    
+    Example usage without LN argument:
+	N = 4
+	Av = [0.1, 0.2, 0.3, 0.4]
+	L = [1, 2, 3, 4]
+
+	table = generate_table(N, Av, L=L)
+
+	for row in table:
+	    print("\t".join(str(x) for x in row))
+
+    """
+    
+    import random
+    import csv
+
+    if L is None:
+        if LN is None:
+            raise ValueError("Either L or LN argument must be provided.")
+        else:
+            L = [LN // N] * N
+            if sum(L) < LN:
+                L[-1] += LN - sum(L) # do not leave out any leaves
+    else:
+        if LN is not None and sum(L) != LN:
+            print("Warning: L argument takes precedence over LN argument.")
+        LN = sum(L)
+
+    # Create list of leaves
+    leaves = [f"Leaf{i}" for i in range(1, LN+1)]
+
+    # Create table
+    table = []
+    for i in range(N):
+        # Select leaves for this row
+        row_leaves = random.sample(leaves, L[i])
+        for leaf in row_leaves:
+            leaves.remove(leaf)
+        # Append row to table
+        row = ["group" + str(i+1), Av[i], ";".join(row_leaves)]
+        table.append(row)
+    
+    table.append([LN, "", ""])
+
+    if outfile is not None:
+        with open(outfile, "w", newline='') as f:
+            writer = csv.writer(f, delimiter='\t')
+            writer.writerow(['Core', 'Average', 'Leaves'])
+            writer.writerows(table)
+    return table
---
+++ 1_datasets/launch_generate_PCGtables.py
@@ -0,0 +1,103 @@
+"""
+This script generates PCG tables in batch. It consists of a loop that calls the `generate_table` function multiple
+times, using different combinations of input arguments. The output tables are saved as CSV files with tab-separated
+values.
+
+Generated files are as follows:
+    For each LN in LN_values (number of species; 10, 100 or 1000):
+          |
+          |-- For each N in N_values[LN] (number of groups; different for each value of LN):
+          |        |
+          |        |-- For each distribution of species between groups (Skewed or Even):
+          |        |         |
+          |        |         |-- For each community file (relative abundance distribution; lognorm or uniform):
+          |        |                 |-- <species_distr>_<comm_distr>_N<N>_LN.csv
+
+So, for every combination of number of total species LN and number of groups N, we will have files like :
+|-- EvenGroups_uniform_N<N>_LN.csv
+|-- SkewedGroups_uniform_N<N>_LN.csv
+|-- EvenGroups_lognorm_N<N>_LN.csv
+|-- SkewedGroups_lognorm_N<N>_LN.csv
+
+Simulations:
+    Each community (abundance table; see "simcomms" folder) will be paired with those PCG tables with the same number of
+    species. This means multiple PCG tables per community. Each community will then be simulated with a varying number of
+    groups + with a varying species distribution between groups (Skewed of Even)
+    [We could also drop lognorm and use uniform communities only, or remake the lognorm ones; see Limitations]
+
+Limitations:
+ - Leaves are randomly and evenly distributed between groups without checking their previous total abundance. If we have
+   a lognormal community of 10 members and we choose to have 3 skewed groups (for instance, with final relative abundances
+   of 70%, 20% and 10%), it might happen that this initial random distribution is very different from the final ones. In
+   this example, group 1's 3 species might happen to sum only 11% of the total abundance, even when they have to grow to
+   constitute the 70%.
+      > This should not pose any serious problems.
+      > However, maybe we should just use uniform communities for simplicity.
+      > Which would mean crear una función nueva en el repo (predicting_fixation/1_datasets/generate_simcomms.R) similar a
+      la de lognorm que ya hay (= un solo PCG) pero capaz de leer una pcg table existente.
+ - No interactions yet.
+      > Adding interactions means using a different script to create interaction tables. After that,
+        we would need to make simulations for all combinations of:
+   	  - Communities (distinct LN)
+   	  - PCG tables  (with the same LN as the communities)
+   	  - Interaction tables (with the same LN as the communities)
+      > Furthermore, we would need to think about which kind of interactions we want (competitive, cooperative...) and
+        between whom (inter-group or intra-group). In this case there would need to be more restrictions about which
+        interaction table can be used for what community.
+
+---------------------------------------------------------------------------------------------------------------------------
+Input variables:
+- LN_values: a list with values for the "Number of Leaves" argument to be used in the `generate_table` function.
+- N_values: a list with values for the "Number of Groups" argument to be used in the `generate_table` function.
+- input_folder: a string with the path to the folder containing the community files. Community files should be named
+    with the format "<GroupDistr>_N<N>_<community_name>_<LN>sp.csv", where <GroupDistr> indicates the way leaves are
+    distributed between groups, <N> is the number of groups, <LN> is the total number of leaves and <community_name>
+    is a unique identifier for the community ("lognorm" or "uniform").
+- output_folder: a string with the path to the folder where the output tables will be saved.
+
+"""
+
+# Import modules
+import os
+import random
+import csv
+
+# Set working directory
+os.chdir('/home/silvia/AAA/2023-02-01_initial_community_simulation')
+
+# Import main function
+from generate_PCGtable import generate_PCGtable
+
+# Set input variables
+LN_values = [10, 100, 1000]
+N_values = {
+    10: [2, 3],
+    100: [2, 3, 5, 10, 25],
+    1000: [2, 3, 5, 10, 25, 50, 100]
+}
+output_folder = 'PCGtables'
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# Start loop
+for LN in LN_values:
+    for N in N_values[LN]:
+        # Generate random Av values
+        for distr in ['uniform', 'lognorm']:
+        
+            if distr == "uniform":
+                Av = [random.uniform(0, 1) for _ in range(N)]
+                total = sum(Av)
+                Av = [v/total for v in Av]
+                av_type = 'EvenGroups'
+            else:
+                mu = random.uniform(-2, 2)
+                sigma = random.uniform(0.1, 1)
+                Av = list(random.lognormvariate(mu, sigma) for _ in range(N))
+                total = sum(Av)
+                Av = [v/total for v in Av]
+                av_type = 'SkewedGroups'
+                
+            # Loop over community files
+            outfile = f'{av_type}_N{N}_{LN}sp'
+            generate_PCGtable(N, Av, LN=LN, outfile=output_folder + '/' + outfile)
