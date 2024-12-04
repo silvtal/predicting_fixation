1c195fa307b9ddf0c12400c222cbe38118fb4355
silvtal <stlvrmrcs@gmail.com>
2024-12-04 17:58:08 +0100

    add scripts for reordering

---
+++ 2_generate_training_data/launch_reorganizar_comunidades.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+CORES=1
+
+DIRECTORIO_BASE=/home/microbios/silvia_backup/silvia/micro/2021_06_28__null_models/results_null_model/2024_simcomms_WITH_GROUPS
+
+# Verificar que el directorio existe
+if [ ! -d "$DIRECTORIO_BASE" ]; then
+    echo "El directorio $DIRECTORIO_BASE no existe."
+    exit 1
+fi
+
+# Iterar sobre todas las carpetas dentro del directorio base
+for carpeta in "$DIRECTORIO_BASE"/SIM*; do
+    if [ -d $carpeta ]; then
+        echo "Procesando carpeta: $carpeta"
+
+	echo '#!/bin/bash' > launch_$(basename $carpeta).sh
+	echo 'cd ..' >> launch_$(basename $carpeta).sh
+
+	echo 'python /ngs/microbios/silvia/reorganizar_comunidades.py '$carpeta >> launch_$(basename $carpeta).sh
+	chmod +x launch_$(basename $carpeta).sh
+
+	qsubmit.pl -q x86_64 -n $CORES --mem 20 -s launch_$(basename $carpeta).sh
+    fi
+done
+
+echo "Procesamiento lanzado para todas las carpetas en $DIRECTORIO_BASE."
+
